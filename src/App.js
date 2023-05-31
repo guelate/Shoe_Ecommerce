2f9a9daebce4ce960beb644a8c67f30f860687d5
kseyo <kseyo@veepee.com>
2023-05-31 15:12:42 +0200

    LAYOUT WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 function App() {
 
   return (
-    <Home />
+    <Shop />
   )
 }
 
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,42 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Carousel from "../components/Carousel";
+import SneakerProductCard from "../components/SneakerProductCard";
+
 const Shop = () => {
 
-}
-export default Shop
+  return (
+    <>
+    {/* modify header title: accueil , homme , femme */}
+      <Header />
+      
+      <SneakerProductCard />
+      <SneakerProductCard />
+      <SneakerProductCard />
+      <SneakerProductCard />
 
-//follow this rules: 
+      <SneakerProductCard />
+      <SneakerProductCard />
+      <SneakerProductCard />
+      <SneakerProductCard />
 
-// work in other branch to try your gitworkflow 
+      <SneakerProductCard />
+      <SneakerProductCard />
+      <SneakerProductCard />
+      <SneakerProductCard />
 
-//trier la boutique : homme / femme / enfant / garçon / fille 
+   
+      <Carousel />
+      <Footer />
+    </>
+  );
+};
+export default Shop;
+
+//follow this rules:
+
+// work in other branch to try your gitworkflow
+
+//trier la boutique : homme / femme 
 //faire fonctionner le defilement de produit(carousel ou autre fonction)
